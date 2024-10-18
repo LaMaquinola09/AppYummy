10da4a95c64dedd0171422b9ad874a73c1d4064f
Victor antonio gomez santiz <victorantoniogomezsantiz8@gmail.com>
2024-10-17 20:33:05 -0600

    Implementacion de Opciones de Registros

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import HomeScreen from './src/screens/User/Home';
 import RegisterScreen from './src/screens/Auth/RegisterScreen';
 import CourierHomeScreen from './src/screens/Courier/CourierHomeScreen';
 import CustomDrawerContent from './src/components/CustomDrawerContent';
+import WelcomeScreen from './src/screens/Auth/WelcomeScreen';
 
 const Stack = createStackNavigator<RootStackParamList>();
 const Drawer = createDrawerNavigator<RootStackParamList>();
@@ -16,54 +17,59 @@ const Drawer = createDrawerNavigator<RootStackParamList>();
 
 // Drawer para las vistas que tendrán menú de hamburguesa
 function DrawerNavigator() {
-    return (
-      <Drawer.Navigator
-        drawerContent={props => <CustomDrawerContent {...props} />}
-        screenOptions={{
-            drawerType:"slide",
-            drawerActiveTintColor: '#fff',
-            drawerInactiveTintColor: '#eee',
-            drawerStyle: {
-                backgroundColor: '#ff6f00', // Color de fondo del drawer
-                width: 240, // Ancho del drawer
-              },
-            
-        }}>
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="CourierHome" component={CourierHomeScreen} />
-      </Drawer.Navigator>
-    );
-  }
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerType: "slide",
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#eee',
+        drawerStyle: {
+          backgroundColor: '#ff6f00', // Color de fondo del drawer
+          width: 240, // Ancho del drawer
+        },
+
+      }}>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="CourierHome" component={CourierHomeScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 // Crea el Stack dentro de una pantalla del Drawer
 function MainStack() {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen
-             name="Login"
-             component={LoginScreen}
-             options={{ headerShown: false }} // Ocultar encabezado
-         />
-         <Stack.Screen
-             name="Register"
-             component={RegisterScreen}
-             options={{ headerShown: false }} // Ocultar encabezado
-         />
-         {/* Vistas con menú de hamburguesa */}
-        <Stack.Screen
-            name="MainDrawer"
-            component={DrawerNavigator}
-            options={{ headerShown: false }} // El Drawer manejará el encabezado
-        />
-      </Stack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }} // Ocultar encabezado
+      />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ headerShown: true, title: 'Bienvenido a YUMMY' }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ headerShown: false }} // Ocultar encabezado
+      />
+      {/* Vistas con menú de hamburguesa */}
+      <Stack.Screen
+        name="MainDrawer"
+        component={DrawerNavigator}
+        options={{ headerShown: false }} // El Drawer manejará el encabezado
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-    return (
-        <NavigationContainer>
-             <MainStack />
+  return (
+    <NavigationContainer>
+      <MainStack />
 
-        </NavigationContainer>
-    );
+    </NavigationContainer>
+  );
 }
--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,8 +1,9 @@
 export type RootStackParamList = {
     Login: undefined;   // Ruta de login
     Home: undefined;    // Ruta de la página principal
-    Register: undefined; // Ruta de registro
+    Register: { userType: 'repartidor' | 'consumidor' }; 
     CourierHome: undefined;
     MainDrawer: undefined;
+    Welcome:undefined;
     // Agrega aquí otras rutas que tengas
   };
